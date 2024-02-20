83b7d7bb303b8c0579ed2ac0a337cf3953f2ab41
LEONARDO VIEIRA GUIMARAES <leonardovieiraxy@hotmail.com>
2024-02-19 23:38:46 -0300

    Changes to be committed: 	new file:   exemplo.rb

---
+++ exemplo.rb
@@ -0,0 +1,20 @@
+# Variáveis Locais
+# Aqui, uma variável local chamada 'nome' está sendo definida com o valor 'João'
+nome = 'João'
+
+print 'Nome: ', nome, "\n"
+
+
+# Instância de variáveis
+# Aqui, uma variável de instância chamada '@idade' está sendo definida com o valor 25
+# Variáveis de instância são acessíveis em qualquer método para a mesma instância de uma classe
+@idade = 25
+
+print 'Idade: ', @idade, "\n"
+
+# Variáveis Globais
+# Aqui, uma variável global chamada '$contador' está sendo definida com o valor 0
+# Variáveis globais são acessíveis de qualquer parte do código Ruby
+$contador = 0
+
+print 'Contador: ', $contador, "\n"
